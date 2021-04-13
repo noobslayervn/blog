5aaadaebbe1285f8ec5f62f20c695ba1365627a7
kienho97 <kien.ho997@gmail.com>
2021-04-13 11:05:51 +0700

    resolve conflict for revert

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,4 @@
 Rails.application.routes.draw do
   root "articles#index"
-  namespace :admin do
-    patch "/admin/articles/:id" => "articles#show", as: "article_path"
-    resources :articles, only: [:new, :create, :index, :show]
-  end
+  resources :articles
 end
