0372220447e3875d568a5810f94e98ccd9624a5f
kienho97 <kien.ho997@gmail.com>
2021-04-16 15:44:00 +0700

    update edit and new using common form

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,19 +5,18 @@ class ArticlesController < ApplicationController
 
   def show
     @article = Article.find(params[:id])
-    @article =  Article.find(1)
   end
   def new
     @article = Article.new
   end
 
   def create
-    @article = Article.new(title: "...", body: "...")
+    @article = Article.new(article_params)
 
     if @article.save
       redirect_to @article
     else
-      render :index
+      render :new
     end
   end
 
